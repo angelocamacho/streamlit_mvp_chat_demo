999f07dce59182c78a76ef7a3210837ec6e6ced7
Joshua-Wacks <joshuawa@bbd.co.za>
2024-02-19 10:29:52 +0200

    Added images to sources

--- chatbot_mvp_demo.py
+++ chatbot_mvp_demo.py
@@ -95,7 +95,7 @@ def setup():
     
 def create_button(btn_key,source_file):
     image_data = get_image_data(source_file)
-    st.image(image_file,output_format="JPEG")
+    st.image(image_data,output_format="JPEG")
 
     st.download_button(
         key = btn_key,
